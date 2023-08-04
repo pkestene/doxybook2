pkestene/doxybook2

commit 1e5f8f0879aa1e908ccce25064ceb7604e1ed94a

Fix pipe in enum member details
Enum values in C++ may be defined by combining multiple other values.

E.g.:
``` cpp
enum class Test {
  None = 0x0,
  A = 0x1,
  B = 0x2,
  C = 0x4,
  All = A | B | C
};
```

Due to the use of the pipe `|`, this will break the table layout for the
description of the enum.
Fix this by escaping any pipes found in the JSON.

src/Doxybook/DefaultTemplates.cpp
@@ -618,7 +618,7 @@ template <{% for param in templateParams %}{{param.typePlain}} {{param.name}}{% 
 | Enumerator | Value | Description |
 | ---------- | ----- | ----------- |
 {% for enumvalue in enumvalues %}| {{enumvalue.name}} | {% if existsIn(enumvalue, "initializer") -%}
-{{replace(enumvalue.initializer, "= ", "")}}{% endif -%}
+{{replace(replace(enumvalue.initializer, "= ", ""), "|", "\\|"))}}{% endif -%}
 | {% if existsIn(enumvalue, "brief") %}{{enumvalue.brief}}{% endif %} {% if existsIn(enumvalue, "details") %}{{enumvalue.details}}{% endif %} |
 {% endfor %}
 {% endif -%}

commit b22f5261bf42d56732d88ea792b71e9942987279

Fix template error introduced in commit 1e5f8f0

src/Doxybook/DefaultTemplates.cpp
@@ -635,7 +635,7 @@ template <{% for param in templateParams %}{{param.typePlain}} {{param.name}}{% 
 | Enumerator | Value | Description |
 | ---------- | ----- | ----------- |
 {% for enumvalue in enumvalues %}| {{enumvalue.name}} | {% if existsIn(enumvalue, "initializer") -%}
-{{replace(replace(enumvalue.initializer, "= ", ""), "|", "\\|"))}}{% endif -%}
+{{replace(replace(enumvalue.initializer, "= ", ""), "|", "\\|")}}{% endif -%}
 | {% if existsIn(enumvalue, "brief") %}{{enumvalue.brief}}{% endif %} {% if existsIn(enumvalue, "details") %}{{enumvalue.details}}{% endif %} |
 {% endfor %}
 {% endif -%}